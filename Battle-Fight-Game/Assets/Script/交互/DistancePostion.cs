a16cbcacbd3ebf1bdd4ee175980b3d1646d2dfbd
ShuiJing9527 <457663161a@gmail.com>
2026-03-09 23:09:27 +0900

    更新

---
+++ Battle-Fight-Game/Assets/Script/交互/DistancePostion.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+
+public class DistancePostion : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("_Position", transform.position);
+    }
+}
