105f868ab213620f423c1e4ec2e63f3cccc61402
ShuiJing9527 <457663161a@gmail.com>
2026-03-12 15:25:56 +0900

    更新

--- Battle-Fight-Game/Assets/Script/交互/DistancePostion.cs
+++ Battle-Fight-Game/Assets/Script/交互/DistancePostion.cs
@@ -14,6 +14,6 @@ public class DistancePostion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Shader.SetGlobalVector("_Position", transform.position);//将当前物体的位置传递给全局着色器变量"_Position"，可以在着色器中使用这个变量来进行相关的计算，例如实现基于距离的效果等
+        Shader.SetGlobalVector("_Position", transform.position);
     }
 }
