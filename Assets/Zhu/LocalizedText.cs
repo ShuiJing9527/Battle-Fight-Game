f56d716d4f96b160ba053f3bd80d0f758a876214
JiangLBei <1354816453@qq.com>
2026-07-03 15:53:40 +0900

    0703

--- Assets/Zhu/LocalizedText.cs
+++ Assets/Zhu/LocalizedText.cs
@@ -3,11 +3,8 @@ using TMPro;
 
 public class LocalizedText : MonoBehaviour
 {
-    [Tooltip("要显示的本地化文本内容")]
     public string showText;
-    [Tooltip("指定TMP中文字体包")]
     public TMP_FontAsset targetFont;
-
     private TextMeshProUGUI tmp;
 
     void Awake()
@@ -17,14 +14,8 @@ public class LocalizedText : MonoBehaviour
 
     void Start()
     {
-        if (tmp == null)
-        {
-            Debug.LogWarning($"物体 {gameObject.name} 未挂载 TextMeshProUGUI 组件", this);
-            return;
-        }
-
+        if (tmp == null) return;
         tmp.text = showText;
-        if (targetFont != null)
-            tmp.font = targetFont;
+        if (targetFont != null) tmp.font = targetFont;
     }
 }
